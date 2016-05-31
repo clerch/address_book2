b7a32240cd54842abdaf598b139d2187969374c6
clerch <lerchc@gmail.com>
2016-05-31 13:45:30 -0700

    database setup

--- db/migrate/20160531195709_create_table_contacts.rb
+++ db/migrate/20160531195709_create_table_contacts.rb
@@ -1,12 +1,13 @@
 class CreateTableContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-    # column definitions go here
-    t.column :contact_id, :integer
-    t.column :first_name, :string
-    t.column :last_name, :string
-    t.column :phone, :string
-    t.column :email, :string
-    t.timestamps
+        # column definitions go here
+        t.column :contact_id, :integer
+        t.column :first_name, :string
+        t.column :last_name, :string
+        t.column :phone, :string
+        t.column :email, :string
+        t.timestamps
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160531195709) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer  "contact_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
